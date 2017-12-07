e8cdf61df817b122413bc3379c72408acecabf46
Benedikt Schmied <github-ce@benedikt-schmied.de>
2017-12-07 15:13:12 +0100

    debug, alert and 'check_arguments' routine

--- setup/development/install_scripts.java.sh
+++ setup/development/install_scripts.java.sh
@@ -1,25 +1,63 @@
 #!/bin/bash
-echo "installing the jave runtime"
 
-if [ $# -ne 2 ]
-then
-        echo "we need the path to the runtime rpm as a parameter - we haven't go it, hence quit!"
-        exit 0
-fi
+# #############################################################################
+# \brief scripts which installs all features in order to run, compile and debug 
+# 	java applications
+#
+# \author benedikt schmied
+# #############################################################################
 
-#we need to be administrator in order to let it run, hence check it
-if [ "$EUID" != "0" ]
-then
-        echo "we need to be administratro - we aren't, hence quit!"
-        exit 0
-fi
 
-if [ -f $2 != 0  ]
-then
-        echo "this is not a valid file, we will quit!"
-        exit 0
-fi
+# #############################################################################
+# \brief return the date
+# #############################################################################
+function do_print_date() {
 
+	echo $(date '+%Y-%m-%d %H:%M:%S')
+}
+
+# #############################################################################
+# \brief prints an alert message
+#
+# \param string
+# #############################################################################
+function do_print_alert() {
+	echo $(do_print_date) " ALERT	" $1 
+}
+
+
+# #############################################################################
+# \brief prints a debugging message
+# #############################################################################
+function do_print_debug() {
+	echo $(do_print_date) " DEBUG	" $1
+}
+
+# #############################################################################
+# \brief checks the scripts arguments
+# #############################################################################
+function do_check_arguments() {
+	do_print_debug " checking the arguments "
+
+	if [ $# -ne 2 ]
+	then
+        	do_print_alert "we need the path to the runtime rpm as a parameter - we haven't go it, hence quit!"
+        	exit 0
+	fi
+
+	#we need to be administrator in order to let it run, hence check it
+	if [ "$EUID" != "0" ]
+	then
+        	do_print_alert "we need to be administratro - we aren't, hence quit!"
+	        exit 0
+	fi
+
+	if [ -f $2 != 0  ]
+	then
+        	do_print_alert "this is not a valid file, we will quit!"
+	        exit 0
+	fi
+}
 
 # #############################################################################
 # 
@@ -72,14 +110,38 @@ function do_install_jre() {
 # #############################################################################
 function do_check_and_run() {
 	
-
+	echo "do check and run"
 }
 
 
+# #############################################################################
+# check, what we have to install either a simple runtime environment 
+# or a development kit
+# #############################################################################
+function do_print_help() {
+	echo $(do_print_date) "install		test"
+	echo $(do_print_date) "-h		help"
+	echo $(do_print_date) "-t		test subfunctions, no install"
+}
+
 case $1 in
-        install)i
+        install)
 		do_check_and_run $2
                 ;;
+	-h)
+		do_print_help
+		;;
+	-t)
+		echo ">>>>> testing date routine"
+		do_print_date
+		echo ">>>>> testing alert"
+		do_print_alert "test run"
+		echo ">>>>> testing debug"
+		do_print_debug "test run"
+		echo ">>>>> checking arguments routine"
+		do_check_arguments $1 $2 $3
+		
+		;;
         *)
                 ;;
 esac
