1423bf837832c47aa3eb55838e249ee694dcc0be
Benedikt Schmied <github-ce@benedikt-schmied.de>
2021-05-01 12:50:54 +0200

    about to restructure

--- maintenance/media/images/rename.sh
+++ maintenance/media/images/rename.sh
@@ -85,62 +85,74 @@ function create_folders() {
 	fi
 }
 
-# find all images (at least all jpegs)
 IFS=$'\n'
-a=$(find $rootnode -iname "*.[jJ][pP]*[gG]")
 
-echo "--- fetched all pictures"
+# #########################################################################
+#
+# #########################################################################
+functions fetch_images_in_input_folder() {
 
-# start processing one after the other
-for i in ${a[@]};
-do
-	filename=${i##*/}
-	extension=${filename##*.}	
-	echo "working on "$i
-	
-	# what will be the filename afterwards?
-	# the formatter is given with the '-d' parameter
-	# we're asking for the DateTimeOriginal
-	# 's3' strips down the information to the pure data (without field identifier such as "Date Time ...")
-    	
-	final_name=$(exiftool -s3 -d "%Y-%m-%d_%H-%M-%S" -DateTimeOriginal $i)
+	# find all images (at least all jpegs)
+	a=$(find $rootnode -iname "*.[jJ][pP]*[gG]")
 
-	# let's introduce a check for the return value here and simply
-	# continue processing in case the return value is zero 
-	# and the given string is unequal ""
-        
-        if [ "$final_name" == "" ];
-	then
-		echo "not able to exif - meta - info from file"	
-		cp $i $faultdir
-	else
+	echo "--- fetched all pictures"
+}
 
-		#final_name=$(exiftool '-DataTimeOriginal $tmpdir/$filename')
-		echo $final_name.$extension
-		if [ ! -f $tmpdir/$final_name.$extension ];
+
+# #########################################################################
+#
+# #########################################################################
+function rename_and_move_into_tmp_or_trash() {
+	# start processing one after the other
+	for i in ${a[@]};
+	do
+		filename=${i##*/}
+		extension=${filename##*.}	
+		echo "working on "$i
+		
+		# what will be the filename afterwards?
+		# the formatter is given with the '-d' parameter
+		# we're asking for the DateTimeOriginal
+		# 's3' strips down the information to the pure data (without field identifier such as "Date Time ...")
+		
+		final_name=$(exiftool -s3 -d "%Y-%m-%d_%H-%M-%S" -DateTimeOriginal $i)
+
+		# let's introduce a check for the return value here and simply
+		# continue processing in case the return value is zero 
+		# and the given string is unequal ""
+		
+		if [ "$final_name" == "" ];
 		then
-			echo "starting copy operation"
-			# we're using a similar command as above for the copy operation
-			# as 'exiftool' will rectify any problem during the copy operation
-			exiftool '-Filename<DateTimeOriginal' -d %Y-%m-%d_%H-%M-%S.%%e $i -o $tmpdir
+			echo "not able to exif - meta - info from file"	
+			cp $i $faultdir
 		else
-			echo "already existing, checking for size"
-			file_sz_existing=$(du -k $i | cut -f1)
-			file_sz_new=$(du -k $tmpdir/$final_name.$extension | cut -f1)
-			echo "new file "$file_sz_new " existing "$file_sz_existing
-			if [ $file_sz_existing -ne $file_sz_new ];
+
+			#final_name=$(exiftool '-DataTimeOriginal $tmpdir/$filename')
+			echo $final_name.$extension
+			if [ ! -f $tmpdir/$final_name.$extension ];
 			then
-				echo "this is a different file, thus, creating a clone"
-				exiftool '-Filename<DateTimeOriginal' -d %Y-%m-%d_%H-%M-%S%%-c.%%e $i -o $tmpdir
+				echo "starting copy operation"
+				# we're using a similar command as above for the copy operation
+				# as 'exiftool' will rectify any problem during the copy operation
+				exiftool '-Filename<DateTimeOriginal' -d %Y-%m-%d_%H-%M-%S.%%e $i -o $tmpdir
+			else
+				echo "already existing, checking for size"
+				file_sz_existing=$(du -k $i | cut -f1)
+				file_sz_new=$(du -k $tmpdir/$final_name.$extension | cut -f1)
+				echo "new file "$file_sz_new " existing "$file_sz_existing
+				if [ $file_sz_existing -ne $file_sz_new ];
+				then
+					echo "this is a different file, thus, creating a clone"
+					exiftool '-Filename<DateTimeOriginal' -d %Y-%m-%d_%H-%M-%S%%-c.%%e $i -o $tmpdir
+				fi
 			fi
-		fi
-	fi;
+		fi;
 
-	# so seems, that we've either could read and copy the file or we've put it into a fault - folder
-	# let's delete it!
-	# rm $i
-done
-
+		# so seems, that we've either could read and copy the file or we've put it into a fault - folder
+		# let's delete it!
+		# rm $i
+	done
+}
 
 # #########################################################################
 #
