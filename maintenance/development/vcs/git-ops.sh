7b895a9657ba86a8959f257ee521daefbb0eab7e
Benedikt Schmied <github-ce@benedikt-schmied.de>
2021-08-07 22:44:11 +0200

    we are now able to create a new repository with this script

--- maintenance/development/vcs/git-ops.sh
+++ maintenance/development/vcs/git-ops.sh
@@ -36,9 +36,65 @@ fi
 
 
 # #########################################################################
-#
+# it will take each folder it can find
+# initialize a git repository
+# add all files to this repository
+# do a initial commit
+# change directory and clone a bore repository which
+# it will move to the server
+# 
+# \param 	$1	name of future repository (of directory)
+# \param	$2	remote git server (that can be accessed via ssh)
 # #########################################################################
-function delete_unnecessary_files() {
+function create_and_populate_and_clone() {
+
+	echo "this it the move to git script"
+
+	main_dir=$PWD
+
+	a=$main_dir/$1
+
+	if [ -d $a ] 
+	then
+		# echo the current directory
+		echo $a
+
+		# go into the directory
+		cd $a
+
+		#extract the folders name -> project name from the current path
+		b=$(basename $a)
+		echo $b
+
+		# make a ignore file
+		echo "*.[ao]" >> .gitignore
+
+		# initialialize the repository
+		git init
+
+		# add all files
+		git add *
+
+		# do an intial commit 
+		git commit -m "initial check in"
+
+		# return from this directory
+		cd $main_dir
+
+		# do a bare clone operation
+		git clone --bare $b $b.git
+
+		# push it to the linux server
+		scp -r $b.git $2
+
+		rm -rf $b $b.git
+
+		#clone it again
+		git clone $2/$b
+
+		# return from this directory
+		cd $main_dir
+	fi
 }
 
 
@@ -68,161 +124,6 @@ case $1 in
 		;;
 esac
 unset IFS
-#!/bin/bash
-# this script works as follows
-# it will take each folder it can find
-# initialize a git repository
-# add all files to this repository
-# do a initial commit
-# change directory and clone a bore repository which
-# it will move to the server
-
-
-echo "this it the move to git script"
-
-main_dir=$PWD
-
-a=$main_dir/$1
-
-if [ -d $a ] 
-then
-# echo the current directory
-echo $a
-
-# go into the directory
-cd $a
-
-#extract the folders name -> project name from the current path
-b=$(basename $a)
-echo $b
-
-# make a ignore file
-echo "*.[ao]" >> .gitignore
-
-# initialialize the repository
-git init
-
-# add all files
-git add *
-
-# do an intial commit 
-git commit -m "initial check in"
-
-# return from this directory
-cd $main_dir
-
-# do a bare clone operation
-git clone --bare $b $b.git
-
-# push it to the linux server
-scp -r $b.git linux-5k9k:/nfsexport/common/vcs/software/
-
-rm -rf $b $b.git
-
-#clone it again
-git clone linux-5k9k:/nfsexport/common/vcs/software/$b
-
-# return from this directory
-cd $main_dir
-
-fi
-#!/bin/bash
-b=$PWD
-for a in ./*;
-do
-if [ -d $a ];
-then
-cd $a;
-echo $a;
-git pull;
-cd $b;
-fi
-done
-
-#!/bin/bash
-b=$PWD
-for a in ./*;
-do
-if [ -d $a ];
-then
-cd $a;
-echo $a;
-git push origin master;
-cd $b;
-fi
-done
-
-#!/bin/bash
-b=$PWD
-for a in ./*;
-do
-if [ -d $a ];
-then
-cd $a;
-echo $a;
-git status;
-cd $b;
-fi
-done
-
-#!/bin/bash
-# this script works as follows
-# it will take each folder it can find
-# initialize a git repository
-# add all files to this repository
-# do a initial commit
-# change directory and clone a bore repository which
-# it will move to the server
-
-
-echo "this it the move to git script"
-
-main_dir=$PWD
-
-a=$main_dir/$1
-
-if [ -d $a ] 
-then
-# echo the current directory
-echo $a
-
-# go into the directory
-cd $a
-
-#extract the folders name -> project name from the current path
-b=$(basename $a)
-echo $b
-
-# make a ignore file
-echo "*.[ao]" >> .gitignore
-
-# initialialize the repository
-git init
-
-# add all files
-git add *
-
-# do an intial commit 
-git commit -m "initial check in"
-
-# return from this directory
-cd $main_dir
-
-# do a bare clone operation
-git clone --bare $b $b.git
-
-# push it to the linux server
-scp -r $b.git linux-5k9k:/nfsexport/common/vcs/software/
-
-rm -rf $b $b.git
-
-#clone it again
-git clone linux-5k9k:/nfsexport/common/vcs/software/$b
-
-# return from this directory
-cd $main_dir
-
-fi
 #!/bin/bash
 b=$PWD
 for a in ./*;
